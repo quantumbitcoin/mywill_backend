ec9d946c7c4b06d2fdc57b83c9bb699db6bec979
Maksim Strenk <ephdtrg@gmail.com>
2019-01-29 16:09:20 +0300

    fix arguments in tests

--- lastwill/payments/api_tests.py
+++ lastwill/payments/api_tests.py
@@ -1,10 +1,81 @@
-from lastwill.profile.models import SubSite
-from exchange_API import convert, to_wish
-from lastwill.consts import NET_DECIMALS
-from lastwill.payments.api import calculate_decimals, add_decimals
+#from lastwill.profile.models import SubSite
+#from exchange_API import convert, to_wish
+#from lastwill.consts import NET_DECIMALS
+#from lastwill.payments.api import calculate_decimals, add_decimals
 import unittest
 import sys
 import ast
+import json
+import requests
+
+NET_DECIMALS = {
+    'ETH': 10 ** 18,
+    'ETH_GAS_PRICE': 10 ** 9,
+    'EOS': 10 ** 4,
+    'WISH': 10 ** 18,
+    'EOSISH': 10 ** 4,
+    'BNB': 10 ** 18,
+    'BTC': 10 ** 8
+}
+
+def convert(fsym, tsyms):
+    eosish_factor = 1.0
+    revesre_convert = False
+    allowed = {'WISH', 'USD', 'ETH', 'EUR', 'BTC', 'NEO', 'EOS', 'EOSISH', 'BNB', 'TRX'}
+    if fsym == 'EOSISH' or tsyms == 'EOSISH':
+        eosish_factor = float(
+        requests.get('https://api.chaince.com/tickers/eosisheos/',
+                     headers={'accept-version': 'v1'}).json()['price']
+        )
+        print('eosish factor', eosish_factor, flush=True)
+        if fsym == 'EOSISH':
+            fsym = 'EOS'
+            if tsyms == fsym:
+                return {'EOS': eosish_factor}
+        else:
+            tsyms = 'EOS'
+            if tsyms == fsym:
+                return {'EOSISH': 1 / eosish_factor}
+            revesre_convert = True
+            eosish_factor = 1 / eosish_factor
+
+    if fsym not in allowed or any([x not in allowed for x in tsyms.split(',')]):
+        raise Exception('currency not allowed')
+    print(fsym, tsyms)
+    answer = json.loads(requests.get(
+        'http://127.0.0.1:5001/convert?fsym={fsym}&tsyms={tsyms}'.format(fsym=fsym, tsyms=tsyms)
+    ).content.decode())
+    print('currency_proxi answer', answer, flush=True)
+    if revesre_convert:
+        answer = {'EOSISH': answer['EOS']}
+        tsyms = 'EOSISH'
+    answer[tsyms] = answer[tsyms] * eosish_factor
+    return answer
+
+
+def to_wish(curr, amount=1):
+    return amount * (convert(curr, 'WISH')['WISH'])
+
+
+def calculate_decimals(currency, amount):
+    # count sum payments without decimals
+    if currency in ['ETH']:
+        amount = amount / NET_DECIMALS['ETH']
+    if currency in ['BTC']:
+        amount = amount / NET_DECIMALS['BTC']
+    if currency in ['EOS']:
+        amount = amount / NET_DECIMALS['EOS']
+    return amount
+
+
+def add_decimals(currency, amount):
+    # add decimals for eth, btc
+    if currency in ['ETH']:
+        amount = amount * NET_DECIMALS['ETH']
+    if currency in ['BTC']:
+        amount = amount * NET_DECIMALS['BTC']
+    return amount
+
 
 
 def from_wish(curr, amount=1):
@@ -29,32 +100,34 @@ def recalculate_amount(amount, currency, site_id):
 
 
 class PaymentsTests(unittest.TestCase):
+    payment_values = []
+
     def setUp(self):
-        self.payment_values = []
-        self.currency = self.payment_values[0]
-        self.amount = float(self.payment_values[1])
-        self.original_amount = float(self.payment_values[2])
-        self.site_id = self.payment_values[3]
+        self.amount = float(self.payment_values[0])
+        self.original_amount = float(self.payment_values[1])
+        self.currency = self.payment_values[2]
+        self.site_id = int(self.payment_values[3])
 
         self.site_one_currencies = ['ETH', 'BTC', 'WISH', 'BNB']
         self.site_two_currencies = ['ETH', 'BTC', 'EOS', 'EOSISH']
         self.common_currencies = ['ETH', 'BTC']
+        self.sites_list = list(range(1,3))
 
     def test_01(self):
         print("check payment belongs to first site")
         if self.currency in self.site_one_currencies:
             if self.currency not in self.common_currencies:
-                assert(self.site_id  == 1)
+                self.assertEqual(self.site_id, 1)
             else:
-                assert(self.site_id in list(range(1,3)))
+                self.assertTrue(self.site_id in self.sites_list)
 
     def test_02(self):
         print("check payment belongs to second site")
         if self.currency in self.site_two_currencies:
             if self.currency not in self.common_currencies:
-                assert(self.site_id  == 2)
+                self.assertEqual(self.site_id, 2)
             else:
-                assert(self.site_id in list(range(1,3)))
+                self.assertTrue(self.site_id in self.sites_list)
 
     def test_03(self):
         if self.site_id != 1:
@@ -116,7 +189,13 @@ class PaymentsTests(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    input_values = ast.literal_eval(sys.argv[1])
+    input_values = []
+    input_values.append(sys.argv.pop())
+    input_values.append(sys.argv.pop())
+    input_values.append(sys.argv.pop())
+    input_values.append(sys.argv.pop())
+    input_values.reverse()
+    print(input_values)
     PaymentsTests.payment_values = input_values
     unittest.main()
 
